bb55bf94d9634b3280b233314f8d626830daa1ee
ktjd1262 <your_email@youremail.com>
2017-09-30 14:34:02 +0900

    Before Eject

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import 'App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h1 className="App-title">PBoilerPlate에 어서오세요</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          모든 것을 이곳에서부터 시작하세요!
         </p>
       </div>
     );
