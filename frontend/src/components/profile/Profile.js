3c31d99ec4bac0ec211d7bb1560f3c13833a9534
ktjd1262 <k9326239@gmail.com>
2017-12-15 17:41:35 +0900

    github

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -4,14 +4,35 @@ import PropTypes from 'prop-types'
 class Profile extends React.Component {
     render() {
         let props = this.props
+
+        let fb = undefined
+        let is = undefined
+        let gb = undefined
+
+        if(props.fbLink !== undefined && props.fbLink !== ""){
+            fb = (<a href={props.fbLink}><img src='/fb.png' alt='' /></a>)
+        }
+
+        if(props.isLink !== undefined && props.isLink !== ""){
+            is = (<a href={props.isLink}><img src='/is.png' alt='' /></a>)
+        }
+
+        if(props.gbLink !== undefined && props.gbLink !== ""){
+            console.log('github!')
+            gb = (<a href={props.gbLink}><img src='/gb.png' alt='' /></a>)
+        }
+
+        console.log(props.gbLink)
+
         return (
             <div className='profile'>
                 <img src={props.imgLink} alt='' />
                 <h3>{props.title}</h3>
                 <p>{props.car}</p>
                 <div className='contact'>
-                    <a href={props.fbLink}><img src='/fb.png' alt='' /></a>
-                    <a href={props.isLink}><img src='/is.png' alt='' /></a>
+                    {fb}
+                    {is}
+                    {gb}
                 </div>
             </div>
         )
@@ -23,14 +44,16 @@ Profile.propTypes = {
     car: PropTypes.string,
     fbLink: PropTypes.string,
     isLink: PropTypes.string,
-    imgLink: PropTypes.string
+    imgLink: PropTypes.string,
+    gbLink: PropTypes.string
 }
 
 Profile.defaultProps = {
     title: '',
     car: '',
     fbLink: '',
-    isLink: ''
+    isLink: '',
+    gbLink: ''
 }
 
 export default Profile;
